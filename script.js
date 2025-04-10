4610f6b9403b5fc893af3bddf958e3e5dd7b475e
enemy114 <swati11452005@gmail.com>
2025-04-10 20:39:04 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function search() {
+  const query = document.getElementById("searchBox").value;
+  if (query.trim() === "") {
+    document.getElementById("result").innerHTML = "Please type something to search.";
+  } else {
+    document.getElementById("result").innerHTML = `You searched for: <b>${query}</b>`;
+  }
+}
